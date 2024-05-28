03bc575c7054385fd35c5ca82584a2c8374b0456
Catherine Bello <bellocatherine022@gmail.com>
2024-05-28 23:05:35 +0800

    update App.js

--- allhome_front-end/src/App.js
+++ allhome_front-end/src/App.js
@@ -2,25 +2,35 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
-import Navigation from "./components/Navigation";
 import ProductPages from './components/ProductPages';
 import ViewCart from './components/ViewCart';
 import ShippingDetails from './components/ShippingDetails';
+import AdminLogin from './components/AdminLogin';
+import AdminPanel from './components/AdminPanel';
+import ManageUser from './components/ManageUser';
+import ManageProduct from './components/ManageProduct';
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [itemQty, setItemQty] = useState([]);
-  const [showNavigation, setShowNavigation] = useState(true);
 
   // initialize new cart
   async function initNewCart() {
     const cartId = JSON.parse(localStorage.getItem('cartId'));
+    const sessionCartId = JSON.parse(sessionStorage.getItem('cartId'));
     let currCartId;
 
-    if (!cartId || cartId === 0 ) {
+    if (sessionCartId) {
+      // assumes that the user is still shopping
+      // and not shipped yet
+      return;
+    } else if (!cartId) {
       currCartId = 1;
       localStorage.setItem('cartId', JSON.stringify(currCartId));
+      sessionStorage.setItem('cartId', JSON.stringify(currCartId));
+    } else if (cartId) {
+      sessionStorage.setItem('cartId', JSON.stringify(cartId));
     }
 
     const currDate = new Date();
@@ -65,31 +75,32 @@ function App() {
 
   // this fetches the items from a cart
   React.useEffect(() => {
-    async function fetchProductList() {
-      try {
-          const response = 
-                    await fetch('http://127.0.0.1:8000/api/product_list', 
-                    {
-                      method: 'GET', 
-                      headers: {'Content-Type': 'application/json'}
-                    });
-
-          if (!response.ok) {
-            throw new Error('Failed to retrieve products');
-          }
-
-          const data = await response.json();
-
-          if (data.status === 200) {
-            setProducts(data.data);
-          }
-      } catch (error) {
-        console.error(`Error: ${error}`);
-      }
-    }
-
     fetchProductList();
-  }, [setProducts]);
+  }, []);
+
+  // this fetches the items from a cart
+  async function fetchProductList() {
+    try {
+        const response = 
+                  await fetch('http://127.0.0.1:8000/api/product_list', 
+                  {
+                    method: 'GET', 
+                    headers: {'Content-Type': 'application/json'}
+                  });
+
+        if (!response.ok) {
+          throw new Error('Failed to retrieve products');
+        }
+
+        const data = await response.json();
+
+        if (data.status === 200) {
+          setProducts(data.data);
+        }
+    } catch (error) {
+      console.error(`Error: ${error}`);
+    }
+  }
 
   // adds item to cart
   function addToCart(productId) {
@@ -149,24 +160,39 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        {showNavigation && <Navigation />}
         <Routes>
           <Route exact path='/' element={<Home initNewCart={initNewCart} />} />
           <Route path='/my-cart'
                 element={<ViewCart cartItems={cartItems} 
                 setCartItems={setCartItems} 
                 products={products}
-                onRemoveFromCart={removeFromCart}
-                setShowNavigation={setShowNavigation} />} 
+                onRemoveFromCart={removeFromCart} />} 
           />
           <Route path='/product-pages'
                 element={<ProductPages products={products}
-                setCarts={addToCart}
-                setShowNavigation={setShowNavigation} />}
+                setCarts={addToCart} />}
           />
           <Route
-            path={'/shipping_details'}
-            element={<ShippingDetails setShowNavigation={setShowNavigation} />}
+            path='/shipping-details'
+            element={<ShippingDetails />}
+          />
+          <Route
+            path='/admin/login'
+            element={<AdminLogin />}
+          />
+          <Route
+            path='/admin/panel'
+            element={<AdminPanel />}
+          />
+          <Route 
+              path="/admin/panel/manage_user"
+              element={<ManageUser />}
+          />
+          <Route
+              path='/admin/panel/manage_product'
+              element={<ManageProduct 
+              products={products} 
+              fetchProductList={fetchProductList} />}
           />
         </Routes>
       </div>
