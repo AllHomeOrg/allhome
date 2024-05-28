51745a9443681cf6f92068c1be4ed133471100ff
Catherine Bello <bellocatherine022@gmail.com>
2024-05-28 22:48:00 +0800

    created AdminPanel

---
+++ allhome_front-end/src/components/AdminPanel.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AdminPanelNavigation from "./AdminPanelNavigation";
+
+function AdminPanel() {
+    const [currAdminSession, setCurrAdminSession] = useState(null);
+    const navigate = useNavigate();
+
+    // turn off navigation
+    React.useEffect(() => {
+        // check if is in session
+        const adminSession = JSON.parse(sessionStorage.getItem('adminSession'));
+        
+        if (!adminSession) {
+            navigate('/admin/login');
+        } else {
+            setCurrAdminSession(adminSession);
+        }
+    }, [navigate]);
+
+    return (
+        <div>
+            <AdminPanelNavigation />
+            {currAdminSession && (
+                <h1>Welcome {currAdminSession.username}!</h1>
+            )}
+        </div>
+    );
+}
+
+export default AdminPanel;
