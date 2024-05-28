9ab33c8af56e12742f7e175054bbb08b6f7819b3
Catherine Bello <bellocatherine022@gmail.com>
2024-05-28 22:46:24 +0800

    update home

--- allhome_front-end/src/components/Home.js
+++ allhome_front-end/src/components/Home.js
@@ -1,29 +1,33 @@
 import allhomelogo from '../assets/allhomelogo.png';
 import { Link } from 'react-router-dom';
+import Navigation from './Navigation';
 function Home({ initNewCart }) {
-    return(
-        <div className='container'>
-            <div className="row justify-content-center mt-5">
-                <div className="col-auto">
-                    <img src={allhomelogo} className="img-fluid" alt='Company Logo' style={{ width: '801px', height: '214px', marginTop: '250px'}}/>
+    return (
+        <>
+            <Navigation />
+            <div className='container'>
+                <div className="row justify-content-center mt-5">
+                    <div className="col-auto">
+                        <img src={allhomelogo} className="img-fluid" alt='Company Logo' style={{ width: '801px', height: '214px', marginTop: '250px' }} />
+                    </div>
                 </div>
-            </div>
-            
-            
-            <div className="row justify-content-center mt-5">
-                <div className="col-auto">
-                    <Link to={'/product-pages'} >
-                        <button className="btn btn-outline-primary btn-lg shop" 
-                                type="button" 
+
+
+                <div className="row justify-content-center mt-5">
+                    <div className="col-auto">
+                        <Link to={'/product-pages'} >
+                            <button
+                                className="btn btn-outline-primary btn-lg shop"
+                                type="button"
                                 onClick={() => initNewCart()}
-                        >
-                            Proceed to Shopping
-                        </button>
-                    </Link>
+                            >
+                                Proceed to Shopping
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        
+        </>
     );
 }
 
